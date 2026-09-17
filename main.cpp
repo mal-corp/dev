21519be5e72f398f2e5db6b557d3a9f2708cc580
mal-corp <malsoft@ya.ru>
2026-09-17 17:23:58 +0300

    Create main.cpp
    add game >< on C++

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main() {
+    // Число генерируется случайным образом от 1 до 100
+    int secretNumber = rand() % 100 + 1;
+    int guess = 0;
+
+    cout << "Угадай число от 1 до 100!" << endl;
+
+    // Цикл работает, пока игрок не угадает
+    while (guess != secretNumber) {
+        cout << "Введи число: ";
+        cin >> guess;
+
+        if (guess > secretNumber) {
+            cout << "Меньше!" << endl;
+        } else if (guess < secretNumber) {
+            cout << "Больше!" << endl;
+        }
+    }
+
+    cout << "Правильно! Ты победил!" << endl;
+    return 0;
+}
